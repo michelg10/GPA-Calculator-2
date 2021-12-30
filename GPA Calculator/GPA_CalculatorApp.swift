91315b286e0e0ffcdfc0e4c198bf1c69d40a6cb6
Michel <legitmichel777@outlook.com>
2021-12-30 15:47:12 +0800

    Initial Commit

---
+++ GPA Calculator/GPA_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPA_CalculatorApp.swift
+//  GPA Calculator
+//
+//  Created by Michel Guo on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct GPA_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
