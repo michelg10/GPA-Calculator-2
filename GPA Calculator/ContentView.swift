67036d4689ef5d89d6765f760a5eb2db708ee74b
Michel <legitmichel777@outlook.com>
2023-05-17 14:12:34 +0800

    Added comments by ChatGPT

--- GPA Calculator/ContentView.swift
+++ GPA Calculator/ContentView.swift
@@ -1,22 +1,22 @@
-//
-//  ContentView.swift
-//  GPA Calculator
-//
-//  Created by Michel Guo on 2021/12/30.
-//
-
 import SwiftUI
 
+// `CapsuleButton` is a reusable view that displays a button with a rounded rectangle (capsule-like) shape.
 struct CapsuleButton: View {
-    var text: String
+    var text: String // The text displayed in the button.
+
     var body: some View {
         ZStack {
+            // The capsule-shaped background of the button.
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(.init("Top Buttons"))
+            
+            // The text displayed in the button.
             Text(text)
                 .font(.system(size: 17, weight: .medium, design: .default))
                 .foregroundColor(.primary)
-        }.frame(width: 143, height: 40)
+        }
+        // The fixed size of the button.
+        .frame(width: 143, height: 40)
     }
 }
 
@@ -33,18 +33,30 @@ struct MaxWidthPreferenceKey: PreferenceKey {
     }
 }
 
-
+// `SubjectDataInputView` is a SwiftUI View representing an individual subject's data input components.
 struct SubjectDataInputView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
+
+    // A singleton instance managing application-wide data.
     var appSingleton: AppSingleton
-    var name: SizeDependentString
-    var subject: Subject
-    var subjectIndex: Int
-    var subjectTitleWidth: CGFloat?
     
+    // `name` is a size-dependent string, providing different text based on the device's horizontal size class.
+    var name: SizeDependentString
+    
+    // The specific `Subject` instance this view is associated with.
+    var subject: Subject
+    
+    // The index of the subject in the global subjects list.
+    var subjectIndex: Int
+    
+    // The width of the subject title text, set externally to match the widest title among all subjects.
+    var subjectTitleWidth: CGFloat?
+
     var body: some View {
         VStack(spacing: 19) {
             HStack(spacing:0) {
+                // The subject's name, displayed as a Text view.
+                // The width of this Text view is set by the `subjectTitleWidth` property to be the maximum of all other subject name views.
                 Text(name.getStringFor(horizontalUserInterfaceSizeClass: horizontalSizeClass))
                     .fixedSize()
                     .font(.system(size: 22, weight: .regular, design: .default))
@@ -54,7 +66,8 @@ struct SubjectDataInputView: View {
                 
                 Spacer(minLength: 0)
                 
-                // The picker for the subject level
+                // A segmented control for selecting the level of the subject.
+                // The user's selection is stored in the `AppSingleton` instance.
                 SegmentedControlView(items: subject.getLevelNames(), selectedIndex: .init(get: {
                     appSingleton.userInput[appSingleton.appliedPresetIndex][subjectIndex].levelIndex
                 }, set: { val in
@@ -64,7 +77,8 @@ struct SubjectDataInputView: View {
                 })).frame(maxWidth: (appSingleton.currentPreset.useSmallLevelDisplay ? 170 : (horizontalSizeClass == .regular ? 470 : 265)))
             }
             
-            // The picker for the score
+            // A picker for the subject score.
+            // The user's selection is stored in the `AppSingleton` instance.
             Picker(selection: .init(get: {
                 appSingleton.userInput[appSingleton.appliedPresetIndex][subjectIndex].scoreIndex
             }, set: { x in
@@ -77,24 +91,24 @@ struct SubjectDataInputView: View {
                     Text(currentScoreAndBaseGPAPair.score.getName(forMode: appSingleton.nameMode)).tag(scoreIndex)
                 }
             } label: {
-                EmptyView()
+                EmptyView() // Picker label is empty as it's not needed in this context.
             }.pickerStyle(SegmentedPickerStyle())
         }.frame(height: 114)
         .padding(.horizontal,11)
     }
 }
 
+// `ContentView` is the primary view of the application, displaying the GPA calculator interface.
 struct ContentView: View {
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
-    @StateObject var appSingleton: AppSingleton
-    @State var navAction:Int? = 0
-    @State var viewInitialized = false
-    @State var subjectTitleWidth: CGFloat?
-    
+    @StateObject var appSingleton: AppSingleton // Singleton instance managing global application data.
+    @State var navAction:Int? = 0 // Navigation action to trigger a navigation link.
+    @State var viewInitialized = false // Flag to check if the view has been initialized.
+    @State var subjectTitleWidth: CGFloat? // The width of the subject title text, set to match the widest title among all subjects.
+
     var body: some View {
         NavigationView {
             VStack(alignment: .center, spacing:0) {
-                
                 // Header
                 Text("GPA Calculator")
                     .font(.system(size: 28, weight: .semibold, design: .default))
@@ -107,18 +121,25 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 14)
                             .foregroundColor(.init("Background"))
                         VStack(spacing: 0) {
+                            // Invisible navigation link triggered by the 'Customize' button.
                             NavigationLink(
-                                destination: CustomizeView(nameMode: $appSingleton.nameMode, appSingleton: appSingleton),tag: 1,selection: $navAction,
+                                destination: CustomizeView(nameMode: $appSingleton.nameMode, appSingleton: appSingleton),
+                                tag: 1,
+                                selection: $navAction,
                                 label: {
                                     EmptyView()
                                 })
+                            
                             HStack(alignment: .center, spacing: 26) {
+                                // Button that triggers the navigation to the 'Customize' view.
                                 Button {
                                     navAction=1
                                     vibrate(.medium)
                                 } label: {
                                     CapsuleButton(text: "Customize")
                                 }.buttonStyle(nilButtonStyle())
+
+                                // Button that resets the user's course input.
                                 Button {
                                     appSingleton.resetUserCourseInput()
                                     vibrate(.medium)
@@ -127,8 +148,8 @@ struct ContentView: View {
                                 }.buttonStyle(nilButtonStyle())
                             }.frame(height: 70)
                             
+                            // Create a `SubjectDataInputView` for each subject in the current preset.
                             let subjects = appSingleton.currentPreset.getSubjects()
-                            
                             ForEach((0..<subjects.count), id: \.self) { subjectIndex in
                                 Rectangle()
                                     .frame(height: 1)
@@ -151,6 +172,7 @@ struct ContentView: View {
                 .padding(.bottom, 20)
                 .navigationBarHidden(true)
                 .onAppear {
+                    // Recompute the GPA and vibrate the device when the view reappears after initialization.
                     if viewInitialized {
                         appSingleton.recomputeGPA()
                         vibrate(.medium)
